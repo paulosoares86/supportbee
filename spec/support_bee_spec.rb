acf54b0b4ca1c322becbe75095415d25a8876ad1
Paulo <phsoares.ita@gmail.com>
2016-02-27 18:08:45 -0300

    refactoring tests

--- spec/support_bee_spec.rb
+++ spec/support_bee_spec.rb
@@ -2,28 +2,28 @@ require './spec/spec_helper'
 require './support_bee'
 require './spec/lib/support_bee'
 
-AUTH_TOKEN = 'pywmQ4yzDnDv3MmK1aix'
-
 describe SupportBee do
-  describe "Tickets" do
 
+  describe "Initialization" do
     it "create an instance without auth_token" do
       expect { SupportBee.new }.to raise_error(MissingTokenException)
     end
 
     it "create an instance without auth_token" do
-      expect { SupportBee.new(auth_token: AUTH_TOKEN) }.to raise_error(MissingSubdomainException)
+      expect { SupportBee.new(auth_token: 'AUTH_TOKEN') }.to raise_error(MissingSubdomainException)
     end
+  end
+
+  describe "Tickets" do
+    let (:client) {SupportBee.new(auth_token: 'AUTH_TOKEN', subdomain: 'xyz')}
 
     it "get the correct base url" do
-      client = SupportBee.new(auth_token: AUTH_TOKEN, subdomain: 'xyz')
       expect(client.get_tickets).to include 'https://xyz.supportbee.com'
     end
 
     it "read tickets" do
-      client = SupportBee.new(auth_token: AUTH_TOKEN, subdomain: 'paulosoares')
-      expect(client.get_tickets.length).to be > 0
+      expect(client.get_tickets).to include :get
+      expect(client.get_tickets).to include 'tickets'
     end
-
   end
 end
